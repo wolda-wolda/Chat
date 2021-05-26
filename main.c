bde21622ac3bc6a6fdd07d8960b2bd1dc29b65ec
Marcel <marcelwalder12@gmail.com>
2021-05-26 11:35:57 +0200

    Start with the Client

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <winsock.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
 
+#define SERVER_PORT 1234
+
+//TCP Client
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int client_socket;
+    struct sockaddr_in server_addr;
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    bzero((char *) &server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(SERVER_PORT);
 }
