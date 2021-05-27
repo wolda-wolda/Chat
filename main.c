6e26286564a6ff41111e0ebfc730bb534cca1f3e
Marcel <marcelwalder12@gmail.com>
2021-05-27 15:26:12 +0200

    Improve TCP Server(able to receive more then one message)

--- main.c
+++ main.c
@@ -24,63 +24,62 @@ static void echo(SOCKET client_socket) {
     char echo_buffer[BUFFER_SIZE];
     int recv_size;
     time_t zeit;
-
-    if ((recv_size = recv(client_socket, echo_buffer, BUFFER_SIZE, 0)) < 0) {
-        printf("Fehler bei recv()");
-    }
-
-    echo_buffer[recv_size] = '\0';
-    time(&zeit);
-    printf("Nachrichten vom Client : %s \t%s", echo_buffer, ctime(&zeit));
+    do {
+        recv_size = recv(client_socket, echo_buffer, BUFFER_SIZE, 0);
+        if (recv_size==0){
+            printf("Client hat die Verbindung getrennt\n");
+        }else{
+            echo_buffer[recv_size] = '\0';
+            time(&zeit);
+            printf("Nachrichten vom Client : %s \t%s", echo_buffer, ctime(&zeit));
+        }
+    } while (recv_size!=0);
 }
 
 //TCP Client
-int main() {
-    SOCKET sock, fd;
-    unsigned int len;
-    struct sockaddr_in server_addr, client;
-    char buffer[1000];
-    int client_addr_len;
-    int retcode;
-    int block;
+    int main() {
+        SOCKET sock, fd;
+        unsigned int len;
+        struct sockaddr_in server_addr, client;
+        char buffer[1000];
+        int client_addr_len;
+        int retcode;
+        int block;
 
-    WORD wVersionRequested = MAKEWORD(1, 1);
-    WSADATA wsaData;
-    WSAStartup(wVersionRequested, &wsaData);
+        WORD wVersionRequested = MAKEWORD(1, 1);
+        WSADATA wsaData;
+        WSAStartup(wVersionRequested, &wsaData);
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        printf("Fehler beim Socket erstellen\n");
-        exit(-1);
-    }
-    bzero((char *) &server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(SERVER_PORT);
-    retcode = bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock < 0) {
+            printf("Fehler beim Socket erstellen\n");
+            exit(-1);
+        }
+        bzero((char *) &server_addr, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+        server_addr.sin_port = htons(SERVER_PORT);
+        retcode = bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
 
 
-    if (retcode < 0) {
-        printf("Verbindung fehlgeschlagen\n");
-        exit(-1);
-    } else {
-        printf("Verbunden!\n");
-        listen(sock, 2);
+        if (retcode < 0) {
+            perror("Socket konnte nicht gebunden werden!\n");
+            exit(-1);
+        } else {
+            printf("Socket erfolgreich erstellt und gebunden!\n");
+            listen(sock, 2);
 
-        for (;;) {
-            len = sizeof(client);
-            fd = accept(sock, (struct sockaddr *) &client, &len);
-            if (fd < 0)
-                perror("Fehler bei accept");
-            printf("Bearbeite den Client mit der Adresse: %s\n",
-                   inet_ntoa(client.sin_addr));
+            for (;;) {
+                len = sizeof(client);
+                fd = accept(sock, (struct sockaddr *) &client, &len);
+                if (fd < 0)
+                    perror("Fehler bei accept");
+                printf("Client verbunden: %s\n", inet_ntoa(client.sin_addr));
 
-            /* Daten vom Client auf dem Bildschirm ausgeben */
-            echo(fd);
-
-            closesocket(fd);
+                /* Daten vom Client auf dem Bildschirm ausgeben */
+                echo(fd);
+                closesocket(fd);
+            }
         }
 
     }
-
-}
