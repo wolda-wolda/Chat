002f0d8fd718391d09ea76df1866cf7af07ba79b
Marcel <marcelwalder12@gmail.com>
2021-06-01 09:45:40 +0200

    client receive

--- main.c
+++ main.c
@@ -10,6 +10,8 @@
 #define BUFFER_SIZE 1000
 #define SERVER_PORT 42069
 
+static void echo(SOCKET client);
+
 static void error_exit(char *errorMessage) {
     fprintf(stderr, "%s: %d\n", errorMessage, WSAGetLastError());
     exit(EXIT_FAILURE);
@@ -27,6 +29,7 @@ void sen(char buffer[BUFFER_SIZE], SOCKET client) {
         time_t zeit;
         time(&zeit);
         printf("An Server gesendet: %s \t%s", buffer, ctime(&zeit));
+        echo(client);
     }
     if (strcmp(buffer, "exit") == 0) {
         closesocket(client);
@@ -53,24 +56,44 @@ void sock() {
     WSAStartup(wVersionRequested, &wsaData);
 
     client = socket(AF_INET, SOCK_STREAM, 0);
-    if (client < 0)
-    {
+    if (client < 0) {
         printf("Fehler beim Socket erstellen\n");
         exit(-1);
     }
-    bzero((char *)&server_addr, sizeof(server_addr));
+    bzero((char *) &server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    retcode = connect(client, (struct sockaddr *)&server_addr,sizeof(server_addr));
-    if (retcode < 0)
-    {
+    server_addr.sin_addr.s_addr = inet_addr("25.104.79.221");
+    retcode = connect(client, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (retcode < 0) {
         printf("Verbindung fehlgeschlagen\n");
         exit(-1);
-    }else{
-        printf("Verbindung zu %s:%hu erfolgreich hergestellt.\n", inet_ntoa(server_addr.sin_addr),ntohs(server_addr.sin_port));
-        do {
-            sen(buffer, client);
-        } while (1);
+    } else {
+        printf("Verbindung zu %s:%hu erfolgreich hergestellt.\n", inet_ntoa(server_addr.sin_addr),
+               ntohs(server_addr.sin_port));
+        sen(buffer, client);
     }
+}
+
+static void echo(SOCKET client) {
+
+    char echo_buffer[BUFFER_SIZE];
+    int recv_size = -1;
+    u_long len;
+    time_t zeit;
+    do {
+        ioctlsocket(client, FIONREAD, &len);
+        if (len) {
+            recv_size = recv(client, echo_buffer, BUFFER_SIZE, 0);
+            if (strcmp(echo_buffer, "exit") == 0) {
+                printf("Client hat die Verbindung getrennt\n");
+                break;
+            } else {
+                echo_buffer[recv_size] = '\0';
+                time(&zeit);
+                printf("Nachrichten vom Client : %s \t%s", echo_buffer, ctime(&zeit));
+                sen(echo_buffer, client);
+            }
+        }
+    } while (recv_size != 0);
 }
