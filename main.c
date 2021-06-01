9f3540c77b612912bf8f756eefb8adfb18e38c4e
hubisohn <unterhubermanuel@gmail.com>
2021-06-01 09:56:11 +0200

    Update: Server can send a message now!
    Signed-off-by: hubisohn <unterhubermanuel@gmail.com>

--- main.c
+++ main.c
@@ -17,18 +17,43 @@ static void error_exit(char *errorMessage) {
     exit(EXIT_FAILURE);
 }
 
+//send mit read buffer
+void sen(SOCKET fd) {
+    int echo_len = 0;
+    char buffer[BUFFER_SIZE];
+
+    bzero(buffer, sizeof *(buffer));
+    printf("Nachricht: ");
+    gets(buffer);
+
+    fflush(stdin);
+    echo_len = strlen(buffer);
+    if (send(fd, buffer, echo_len, 0) != echo_len) {
+        error_exit("send() hat eine andere Anzahl von Bytes versendet als erwartet !!!!");
+    } else {
+        time_t zeit;
+        time(&zeit);
+        printf("An Server gesendet: %s \t%s", buffer, ctime(&zeit));
+    }
+    if (strcmp(buffer, "exit") == 0) {
+        closesocket(fd);
+        exit(0);
+    }
+}
+
 static void echo(SOCKET);
 
-static void echo(SOCKET client_socket) {
+static void echo(SOCKET fd) {
 
     char echo_buffer[BUFFER_SIZE];
     int recv_size = -1;
     u_long len;
     time_t zeit;
+
     do {
-        ioctlsocket(client_socket, FIONREAD, &len);
+        ioctlsocket(fd, FIONREAD, &len);
         if (len) {
-            recv_size = recv(client_socket, echo_buffer, BUFFER_SIZE, 0);
+            recv_size = recv(fd, echo_buffer, BUFFER_SIZE, 0);
             if (strcmp(echo_buffer, "exit") == 0) {
                 printf("Client hat die Verbindung getrennt\n");
                 break;
@@ -36,8 +61,10 @@ static void echo(SOCKET client_socket) {
                 echo_buffer[recv_size] = '\0';
                 time(&zeit);
                 printf("Nachrichten vom Client : %s \t%s", echo_buffer, ctime(&zeit));
+                sen(fd);
             }
         }
+
     } while (recv_size != 0);
 }
 
